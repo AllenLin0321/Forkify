7cf968f2796f1de9c66f5c965a9f50b7ed4cf62c
Allen <glux7381@gmail.com>
2019-04-09 14:46:46 +0800

    Build the Recipe View

--- src/js/index.js
+++ src/js/index.js
@@ -83,6 +83,9 @@ const controlRecipe = async () => {
         recipeView.clearRecipe();
         renderLoader(elements.recipe);
 
+        // High-light selected search item
+        searchView.highlightSelected(id);
+
         // Create new recipe object
         state.recipe = new Recipe(id);
 
